56dc0685c5234112eac0e7a4677cafacdd190a6a
sshikhar1234 <sshikharshah@gmail.com>
2019-11-07 13:01:23 -0500

    R4 Working. ALL DONE

--- app/src/main/java/project/university/marking_itus_android/MainActivity.java
+++ app/src/main/java/project/university/marking_itus_android/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.SeekBar;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,8 +14,7 @@ import androidx.appcompat.widget.AppCompatSeekBar;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.RejectedExecutionException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,26 +29,32 @@ public class MainActivity extends AppCompatActivity {
 
     int counter = 0;
     String TAG = "Tag";
-    private final String PARTICLE_USERNAME = "sshikharshah@gmail.com";
-    private final String PARTICLE_PASSWORD = "Particle_2022";
+    int currentProgress = 0;
+    int prevProgress = 0;
+
+    int UPDATE_TIME_PERIOD = 2000;
 
     // MARK: Particle device-specific info
+    private final String PARTICLE_USERNAME = "sshikharshah@gmail.com";
+    private final String PARTICLE_PASSWORD = "Particle_2022";
     private final String DEVICE_ID = "3e0031001447363333343437";
+    private ParticleDevice mDevice;
+
+    //Setup background thread for updating time
+    final Handler childHandler = new Handler();
+    Runnable periodicUpdate;
+
+
     @BindView(R.id.button)
     Button button;
-    Boolean started = false;
     @BindView(R.id.appCompatSeekBar)
     AppCompatSeekBar appCompatSeekBar;
     @BindView(R.id.textView)
     TextView textView;
-    @BindView(R.id.tvTimer)
-    TextView tvTimer;
-    final int UPDATE_TIME_PERIOD = 1000;
-    private ParticleDevice mDevice;
-    //Setup background thread for updating time
-    final Handler childHandler = new Handler();
-     Runnable periodicUpdate;
-
+    @BindView(R.id.slowDownTime)
+    TextView slowDownTime;
+    @BindView(R.id.textViewTimerUpdate)
+    TextView textViewTimerUpdate;
 
 
     @Override
@@ -67,20 +71,27 @@ public class MainActivity extends AppCompatActivity {
         appCompatSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                Log.d(TAG, "onProgressChanged: ");
+
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 Log.d(TAG, "onStartTrackingTouch: ");
+                //Remove the previous callbacks
+                childHandler.removeCallbacksAndMessages(null);
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                Log.d(TAG, "onStopTrackingTouch: ");
+                    int i = seekBar.getProgress();
+                    Log.d(TAG, "onStopTrackingTouch: progress "+ seekBar.getProgress());
+                    UPDATE_TIME_PERIOD = 2000+i;
+                    slowDownTime.setText(String.format("Time slows down by : %d miliseconds", i));
+                    Log.d(TAG, "onStopTrackingTouch: new update time "+ UPDATE_TIME_PERIOD);
+                    //Add new callback with updated interval time
+                    childHandler.postDelayed(periodicUpdate, UPDATE_TIME_PERIOD);
             }
         });
-
     }
 
     public static boolean isBetween(int x, int lower, int upper) {
@@ -109,40 +120,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     //Function to call Particle's Exposed Function
-    private void callParticleFunction(final String functionName){
-        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
-            @Override
-            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
-                Log.d(TAG, "Availble functions: " + mDevice.getFunctions());
-                List<String> parameters = new ArrayList<>();
-                try {
-                    mDevice.callFunction(functionName,parameters);
-                } catch (ParticleDevice.FunctionDoesNotExistException e) {
-                    e.printStackTrace();
+    private void callParticleFunction(final String functionName) throws RejectedExecutionException {
+        try {
+
+            Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+                @Override
+                public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                    Log.d(TAG, "Availble functions: " + mDevice.getFunctions());
+                    List<String> parameters = new ArrayList<>();
+                    try {
+                        mDevice.callFunction(functionName, parameters);
+                    } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                        e.printStackTrace();
+                    }
+                    return -1;
                 }
-                return -1;
-            }
 
-            @Override
-            public void onSuccess(Object o) {
-                // put your success message here
-                Log.d(TAG, "Success!");
-            }
+                @Override
+                public void onSuccess(Object o) {
+                    // put your success message here
+                    Log.d(TAG, "Success!");
+                }
 
-            @Override
-            public void onFailure(ParticleCloudException exception) {
-                // put your error handling code here
-                Log.d(TAG, exception.getBestMessage());
-            }
-        });
+                @Override
+                public void onFailure(ParticleCloudException exception) {
+                    // put your error handling code here
+                    Log.d(TAG, exception.getBestMessage());
+                }
+            });
+        } catch (RejectedExecutionException e) {
+            Log.d(TAG, "callParticleFunction: "+e.getLocalizedMessage());
+        }
     }
 
     private void updateUIandCallFunction() {
-        if(counter>=0 && counter<20) {
+
+        if (counter >= 0 && counter < 20) {
 
             counter = counter + 1;
-            tvTimer.setText("Time: " + this.counter);
+            textViewTimerUpdate.setText(""+counter);
+//            textViewTimer.setText(this.counter);
             if (isBetween(counter, 0, 2)) {
                 callParticleFunction("Smile6");
 //Smile6
@@ -153,13 +172,13 @@ public class MainActivity extends AppCompatActivity {
                 callParticleFunction("Smile4");
 
             }
-            if (isBetween(counter, 6, 8)) {
+            if (isBetween(counter, 6, 10)) {
 //Smile3
                 Log.d(TAG, "decreaseTimer: 3");
                 callParticleFunction("Smile3");
 
             }
-            if (isBetween(counter, 9, 13)) {
+            if (isBetween(counter, 11, 13)) {
 //Smile2
                 Log.d(TAG, "decreaseTimer: 2");
 
@@ -172,7 +191,7 @@ public class MainActivity extends AppCompatActivity {
             if (isBetween(counter, 16, 17)) {
 //anger1
                 callParticleFunction("Anger1");
-            } else if (isBetween(counter, 18, 19    )) {
+            } else if (isBetween(counter, 18, 19)) {
 //anger2
                 callParticleFunction("Anger2");
 
@@ -181,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        if(counter == 20){
+        if (counter == 20) {
             callParticleFunction("resetAll");
         }
     }
@@ -189,12 +208,15 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.button)
     public void onViewClicked() {
         Log.d(TAG, "onViewClicked: ");
-        periodicUpdate  = () -> {
+        periodicUpdate = () -> {
             Log.d(TAG, "insideHandler: ");
             updateUIandCallFunction();
-            childHandler.postDelayed(periodicUpdate,UPDATE_TIME_PERIOD);
+            childHandler.postDelayed(periodicUpdate, UPDATE_TIME_PERIOD);
+            textViewTimerUpdate = findViewById(R.id.textViewTimerUpdate);
+//            Log.d(TAG, "onViewClicked: "+textViewTimerUpdate.setText("as"););
+           // textViewTimerUpdate.setText(" "+counter);
         };
-        childHandler.postDelayed(periodicUpdate,UPDATE_TIME_PERIOD);
+        childHandler.postDelayed(periodicUpdate, UPDATE_TIME_PERIOD);
 
     }
 
